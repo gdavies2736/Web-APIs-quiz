758e53bccb7dc687994741aee2d8c4607cbd9f89
GazVanc <gdavies2736@gmail.com>
2023-11-08 19:12:27 +0000

    started to added pseudo code to js file

---
+++ script.js
@@ -0,0 +1,20 @@
+//Timer function
+
+//View highscores
+
+
+//**Layout**/
+
+//How to change from first page into questions - options
+//1. Set Coding quiz as header.
+//2.
+
+//**How it's going to work**/
+
+//Each answer is assigned a numeric value
+//if correct answer selected:
+//1. Display well done message 
+//2. get numeric value of correct answer
+//3. Add numeric number to an array
+//4. Assign +/- seconds value to each answer
+//5. +/- seconds from timer
