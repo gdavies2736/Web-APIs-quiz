e41cf2fe97f92c79a4e6da31dea5b18c378689f1
GazVanc <gdavies2736@gmail.com>
2023-11-21 21:00:23 +0000

    added event listener for starter button, function for countdown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,29 @@
-//Timer function
 
-//View highscores
+//Create variables. Use HTML to check id's.
 
-
-//Create questions for quiz inside variable
-
-var timer = document.getElementById("time");
-var time = 75;
+var countdown = document.getElementById("time");
+var timeLeft = 75;
+var timer;
 var score = 0;
 var highScores = [];
 var i = 0;
 var startButton = document.getElementById("start");
 var startScreen = document.getElementById("start-screen");
 var questionScreen = document.getElementById("questions");
-var questionTitle = document.getElementByID("question-title");
-var choiceA = document.getElementByID("choiceA");
-var choiceB = document.getElementByID("choiceB");
-var choiceC = document.getElementByID("choiceC");
-var choiceD = document.getElementByID("choiceD");
-var endScreen = document.getElementByID("end-screen");
-var finalScore = document.getElementByID("final-score");
-var submitButton = document.getElementByID("submit");
+var questionTitle = document.getElementById("question-title");
+var choices = document.getElementById("choices");
+var choiceA = document.getElementById("choiceA");
+var choiceB = document.getElementById("choiceB");
+var choiceC = document.getElementById("choiceC");
+var choiceD = document.getElementById("choiceD");
+var endScreen = document.getElementById("end-screen");
+var finalScore = document.getElementById("final-score");
+var submitButton = document.getElementById("submit");
 var enterInitials = document.getElementById("initials");
 var feedback = document.getElementById("feedback");
+
+//Create questions for quiz inside variable
+//Should contain question title, options and answer.
 
 var questions = [ 
  {
@@ -32,19 +33,19 @@ answer: "console.log()"
 },
 {
  title:"Which of the following statements creates a variable in Javascript?",
- options: ["ver", "var", "rav", "vra"] 
+ options: ["ver", "var", "rav", "vra"], 
  answer: "var"  
 },
 {
 title: "What does iteration mean?",
 options: ["The repetition of a process", "The end of a process", "The start of a process", "It only happens once"],
-answer: "The repetition of a process"
-}
+answer: "The repetition of a process",
+},
 {
 title: "Which method removes the last element from an array and returns that element?",
 options: ["shift()", "undo()", "unshift()", "pop()" ],
-answer: "pop()"
-}
+answer: "pop()",
+},
 {
 title: "Which of these operators checks whether its two operands are note equal?",
 options: ["===", "!=", "!==", "+="],
@@ -52,7 +53,61 @@ answer: "!==",
 }
 ]
 
-//button id="start"
+//Start the quiz - on click of start button, first quiz question should appear
+//timer should begin on click of start button.
+//create timer 
+//Need intro text to disappear and question and answer to appear.
+
+//Countdown timer function
+//From above - var countdown = document.getElementById("time");
+//From above - var timeLeft = 75;
+
+function startGame () {
+startScreen.style.display = "none";
+questionScreen.classList.remove("hide");
+startTimer()
+displayQuestion ()
+}
+
+function startTimer () {
+timer = setInterval(function(){
+if (timeLeft > 0){
+timeLeft -= 1
+countdown.innerText = timeLeft
+}   
+}, 1000)
+}
+
+function displayQuestion () {
+questionTitle.innerText = questions[i].title;
+choiceA.innerText = questions[i].options[0];
+choiceB.innerText = questions[i].options[1];
+choiceC.innerText = questions[i].options[2];
+choiceD.innerText = questions[i].options[3];
+}
+
+/*document.getElementById("time").textContent = timeLeft;
+if (timeLeft <=0) {
+
+};*/
+
+startButton.addEventListener("click", startGame);
+
+choices.addEventListener("click", checkAnswer);
+
+function checkAnswer (event) {
+var choice = event.target.innerText;
+var correctAnswer = questions[i].answer;
+if (choice !== correctAnswer) {
+timeLeft -= 5;
+}
+i++;
+if (i >= questions.length) {
+questionScreen.classList.add("hide");
+return;
+}
+displayQuestion ();
+}
 
 //On click - start quiz - X random selection, would it be a for loop to work through each question?
 //On click needs to be on every button
